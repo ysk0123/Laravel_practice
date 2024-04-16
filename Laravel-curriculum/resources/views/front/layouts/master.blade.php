00cd1e4c9220e1aaebfa9230b4f0c381da4290ce
yusuke sato <bavardage4@gmail.com>
2024-04-17 00:52:56 +0900

    update -top.blade.php and about.blade.php templates using Blade directives

--- Laravel-curriculum/resources/views/front/layouts/master.blade.php
+++ Laravel-curriculum/resources/views/front/layouts/master.blade.php
@@ -6,34 +6,18 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta name="expires" content="604800">
   <meta name="format-detection" content="email=no,telephone=no,address=no">
-  <title>トップページ | Laravel</title>
-  <meta name="description" content="トップページのdescriptionです">
+  <title>@yield('title')| Laravel</title>
+  <meta name="description" content="@yield('description','トップページのdescriptionです')">
   <link rel="stylesheet" href="{{ asset('assets/style.css')}}">
 </head>
 <body>
   <header>
-    <div class="header">
-      <div class="logo">
-        <img src="./assets/images/logo.png" alt="ロゴ">
-      </div>
-      <nav>
-        <ul class="header-nav">
-          <li><a href="http://localhost" class="nav_link">home</a></li>
-          <li><a href="{{ route('about')}}" class="nav_link">about</a></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+@include('front.partials.header')
   <main>
     <div class="main">
-      <h1 class="page-name">トップページです。</h1>
-    </div>
+    @yield('content')
+</div>
   </main>
-  <footer>
-    <div class="footer">
-      <p class="footer-txt">フッターです。</p>
-    </div>
-  </footer>
+@include('front.partials.footer')
 </body>
-</html>
 </html>
