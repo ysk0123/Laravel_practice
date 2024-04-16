d65fcad0421fede2dcc23e67547f1c75d8592254
yusuke sato <bavardage4@gmail.com>
2024-04-16 18:54:12 +0900

    [update] Created TopController to handle top page

--- Laravel-curriculum/routes/web.php
+++ Laravel-curriculum/routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopController;
 use App\Http\Controllers\AboutController;
 // Route::get('/', function () {
     // return view('welcome');
 // });
 
-Route::get('/', function(){
-    return view('front.page.top');
-});
+Route::get('/', [TopController::class, 'top'])->name('top');
 Route::get('/about', [AboutController::class, 'about'])->name('about');
