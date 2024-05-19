f8e9d63b001f1f23c68814bb3d27055a2ae69ecb
yusuke sato <bavardage4@gmail.com>
2024-05-19 13:31:10 +0900

    wip

--- Laravel-curriculum/app/Models/User.php
+++ Laravel-curriculum/app/Models/User.php
@@ -50,8 +50,4 @@ class User extends Authenticatable
         return $this->belongsTo(Company::class, 'company_id');
     }
 
-    public function books()
-    {
-        return $this->hasMany(Book::class, 'author_id');
-    }
 }
